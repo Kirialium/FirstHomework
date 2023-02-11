bae73e7283a085b83ac37ecbdd46336b60a05838
Kirill <csgokirilll@mail.ru>
2023-02-11 17:00:30 +0500

    ctrate MainActivity

--- app/src/main/java/com/example/businesswardrobe/MainActivity.java
+++ app/src/main/java/com/example/businesswardrobe/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.businesswardrobe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -9,8 +13,8 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    final EditText editTextMoney = (EditText) findViewById(R.id.edit_text_count_money);
-
+    private Button btn_ok;
+    private EditText edit_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,16 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(this.getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
 
-        String value = editTextMoney.getText().toString();
-        int finalValue = Integer.parseInt(value);
+        btn_ok = findViewById(R.id.button_ok);
+        edit_count = findViewById(R.id.edit_text_count_money);
 
-        
-
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ShowToast")
+            @Override
+            public void onClick(View view) {
+                int fart = Integer.parseInt(edit_count.getText().toString());
+                
+            }
+        });
     }
 }
