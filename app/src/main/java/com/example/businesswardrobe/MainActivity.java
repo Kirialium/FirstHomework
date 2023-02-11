62df124ae2a8237a028d083a60debd4335412aae
Kirill <csgokirilll@mail.ru>
2023-02-11 22:22:50 +0500

    pochti

--- app/src/main/java/com/example/businesswardrobe/MainActivity.java
+++ app/src/main/java/com/example/businesswardrobe/MainActivity.java
@@ -18,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_ok;
     private EditText edit_count;
     private TextView youCan;
+    private int money;
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView4;
+    private TextView textView5;
+    private TextView textView6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +34,11 @@ public class MainActivity extends AppCompatActivity {
         btn_ok = findViewById(R.id.button_ok);
         edit_count = findViewById(R.id.edit_text_count_money);
         youCan = findViewById(R.id.you_can);
-
-        fillAssortment();
+        textView2 = findViewById(R.id.text_view2);
+        textView3 = findViewById(R.id.text_view3);
+        textView4 = findViewById(R.id.text_view4);
+        textView5 = findViewById(R.id.text_view5);
+        textView6 = findViewById(R.id.text_view6);
 
         btn_ok.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("ShowToast")
@@ -38,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
                 if(TextUtils.isEmpty(edit_count.getText().toString())){
                     Toast.makeText(getApplicationContext(), "Type count", Toast.LENGTH_SHORT).show();
                 }else {
-                    int money = Integer.parseInt(edit_count.getText().toString());
+                    money = Integer.parseInt(edit_count.getText().toString());
                     youCan.setAlpha(1);
+                    fillAssortment(money);
                 }
             }
         });
@@ -48,36 +58,107 @@ public class MainActivity extends AppCompatActivity {
         String name;
         int cost;
         float discount;
+        float total;
     }
-    public void fillAssortment(){
+    public void fillAssortment(int money){
         //Пальто
         assortment coat = new assortment();
         coat.name = "Coat";
         coat.cost = 70;
         coat.discount = 0.77f;
+        coat.total = coat.cost * coat.discount;
 
         //Шляпа
         assortment hat = new assortment();
         hat.name = "Hat";
         hat.cost = 25;
         hat.discount = 0.37f;
+        hat.total = hat.cost * hat.discount;
 
         //Костюм
         assortment suit = new assortment();
         suit.name = "Suit";
         suit.cost =  53;
         suit.discount = 0.44f;
+        suit.total = suit.cost * suit.discount;
 
         //Сорочка
         assortment shirt = new assortment();
         shirt.name = "Shirt";
         shirt.cost = 19;
         shirt.discount = (int)1;
+        shirt.total = shirt.cost * shirt.discount;
 
         //Туфли
         assortment shoes = new assortment();
         shoes.name = "Shoes";
         shoes.cost = 41;
         shoes.discount = 0.32f;
+        shoes.total = shoes.cost * shoes.discount;
+
+        float fullCost = coat.total + hat.total + suit.total + shirt.total + shoes.total;
+
+        if(fullCost <= money){
+            textView2.setAlpha(1);
+            textView2.setText("Вы можете купить весь набор!");
+        }
+        else if(shirt.total > money){
+            textView2.setAlpha(1);
+            textView2.setText("Вы не можете купить ничего");
+        }
+        else{
+            textView2.setAlpha(1);
+            textView2.setText("Вы не можете купить весь набор, но вы можете купить это по отдельности:");
+            int sum = money - (int)shirt.total;
+            int count = 1;
+            while(count <= 4){
+                switch(count){
+                    case 1:
+                        sum = sum - (int)hat.total;
+                        textView3.setAlpha(1);
+                        textView3.setText("Сорочка");
+                        if(money - sum >= 0){
+                            count++;
+                        }
+                        else{
+                            break;
+                        }
+                        break;
+                    case 2:
+                        sum = sum - (int)shoes.total;
+                        textView4.setAlpha(1);
+                        textView4.setText("Туфли(32% скидка!)");
+                        if(money - sum >= 0){
+                            count++;
+                        }
+                        else{
+                            break;
+                        }
+                        break;
+                    case 3:
+                        sum = sum - (int)suit.total;
+                        textView5.setAlpha(1);
+                        textView5.setText("Костюм(44% скидка!)");
+                        if(money - sum >= 0){
+                            count++;
+                        }
+                        else{
+                            break;
+                        }
+                        break;
+                    case 4:
+                        sum = sum - (int)coat.total;
+                        textView6.setAlpha(1);
+                        textView6.setText("Пальто(77% скидка!)");
+                        if(money - sum >= 0){
+                            count++;
+                        }
+                        else{
+                            break;
+                        }
+                        break;
+                }
+            }
+        }
     }
 }
